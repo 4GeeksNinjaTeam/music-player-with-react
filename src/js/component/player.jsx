0cf396039eaa54f1b8f8ad68c67eb59715617b4d
Andres Campopiano <acampopiano@gmail.com>
2021-05-04 14:13:39 +0000

    subo archivo

--- src/js/component/player.jsx
+++ src/js/component/player.jsx
@@ -5,6 +5,7 @@ export const Player = props => {
 	const inputRef = useRef();
 	const [state, setState] = useState(false);
 	const [play_pause, setPlayPause] = useState("play");
+
 	const togglePlay = () => {
 		setState(!state);
 		if (state) {
@@ -15,9 +16,10 @@ export const Player = props => {
 			return inputRef.current.play();
 		}
 	};
+
 	return (
 		<>
-			<audio ref={inputRef} src={props.song} />
+			<audio ref={inputRef} src={props.song} autoPlay />
 			<button
 				onClick={togglePlay}
 				className={`${play_pause}-button`}></button>
