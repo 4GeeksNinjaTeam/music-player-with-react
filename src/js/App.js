5f2dd84a7027982c4d02590e9b0caf5f8f6808f0
Andres Campopiano <acampopiano@gmail.com>
2021-05-05 11:34:48 +0000

    subo fetch funcionando

--- src/js/App.js
+++ src/js/App.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import { SoundList } from "./component/soundlist.jsx";
 import { Player } from "./component/player.jsx";
 
-//import JsonData from "../data/response.json";
-//import axios from "axios";
 const App = () => {
 	const songURL = "https://assets.breatheco.de/apis/sound/";
 	const [currentSong, setCurrentSong] = useState("");
@@ -14,31 +12,12 @@ const App = () => {
 				let response = await fetch(songURL + "songs");
 				let responseJson = await response.json();
 				setSoundList(responseJson);
-			} catch (error) {
-				console.error(error);
+			} catch (e) {
+				//console.error(e);
 			}
 		};
 		fetchData();
 	}, []);
-	//console.log(soundList);
-	/*const [soundList, setSoundList] = useState({ Songs: [] });
-	const [JSONUrl, setJSONUrl] = useState(
-		"https://assets.breatheco.de/apis/sound/songs"
-	);
-	//	"https://assets.breatheco.de/apis/sound/data/songs.json"
-
-	useEffect(() => {
-		const fetchData = async () => {
-			const response = await axios.get(JSONUrl);
-			//const result = await JSON.parse(response); //response.json();
-			setSoundList(response);
-		};
-		fetchData();
-	}, []);
-    */
-	/*useEffect(() => {
-		setSoundList(fetchData);
-	}, []);*/
 
 	const playSound = url => {
 		setCurrentSong(url);
